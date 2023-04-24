23190fd2c5487d71ff3b9f3703dad6ba74146b0b
faleialina <august3@mail.ru>
2023-04-24 13:47:27 +0300

    environment-json-file

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { getAll, createEnvironment, deleteEnvironment } = require('./service/service.js');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    try {
+        const data = getAll();
+        res.send(data);
+    } catch (error) {
+        res.send(error.message)
+    }
+});
+
+app.post('/', function (req, res) {
+    try {
+        const { label, category, priority } = req.body;
+        const data = createEnvironment(label, category, priority);
+        res.send(data);
+    } catch (error) {
+        res.send(error.message)
+    }
+});
+
+app.delete('/:id', function (req, res) {
+    try {
+        const { id } = req.params;
+        const data = deleteEnvironment(id);
+        res.send(data);
+    } catch (error) {
+        res.send(error.message)
+    }
+});
+
+
+app.listen(3000, () => console.log('Server is running'));
---
+++ service/service.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+const path = './storage/storage.json';
+
+function getAll() {
+    const arr = JSON.parse(fs.readFileSync(path));
+    if (!arr.length) throw new Error('массив пуст');
+    return arr;
+}
+
+function createEnvironment(label, category, priority) {
+    const arr = JSON.parse(fs.readFileSync(path));
+    arr.push({
+        id: label.toLowerCase(),
+        label: label,
+        category: category,
+        priority: priority
+    });
+    fs.writeFileSync(path, JSON.stringify(arr))
+    return arr;
+};
+
+function deleteEnvironment(id) {
+    const arr = JSON.parse(fs.readFileSync(path));
+    const filtered = arr.filter((elem) => elem.id != id);
+    fs.writeFileSync(path, JSON.stringify(filtered))
+    return filtered;
+};
+
+module.exports = { getAll, createEnvironment, deleteEnvironment };
